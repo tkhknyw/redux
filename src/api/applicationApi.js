53d52016c8695599e0651e62703d0c6c812ec640
tkhknyw <haolong66@gmail.com>
2017-08-09 09:59:01 -0500

    more form

---
+++ src/api/applicationApi.js
@@ -0,0 +1,10 @@
+class ApplicationApi {
+  static getAllApplications() {
+    return fetch('http://localhost:5000/api/v1/cats').then(response => {
+      return response.json();
+    }).catch(error => {
+      return error;
+    });
+  }
+
+  export default ApplicationApi;
